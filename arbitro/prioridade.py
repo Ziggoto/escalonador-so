4586938736ad5392557b4914a1e44d1471ff8e26
Ziggoto <fabiotheophilo@gmail.com>
2014-09-12 22:25:44 -0300

    Corrigido alguns bugs

--- arbitro/prioridade.py
+++ arbitro/prioridade.py
@@ -11,19 +11,16 @@ class FilaPrioridade(arbitro.round_robin.RoundRobin):
         f2 = [] # Fila de media prioridade
         f3 = [] # Fila de baixa prioridade
         f4 = [] # Fila de baixissima prioridade
-        p = None
         
-        for i in range(len(self.aptos)):
-            p = self.aptos[i]
-            
-            if p.prioridade == 0:
-                f1.append(p)
-            elif p.prioridade == 1:
-                f2.append(p)
-            elif p.prioridade == 2:
-                f3.append(p)
+        for i in range(self.aptos):
+            if i.prioridade == 0:
+                f1.append(i)
+            elif i.prioridade == 1:
+                f2.append(i)
+            elif i.prioridade == 2:
+                f3.append(i)
             else:
-                f4.append(p)
+                f4.append(i)
             
         fila_final = f1 + f2 + f3 + f4
         self.aptos = fila_final
